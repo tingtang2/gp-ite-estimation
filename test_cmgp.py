e18dd4fb3cc9d2a53d372de3dac684470870e5b3
Ting Chen <zc2666@columbia.edu>
2024-12-02 11:40:42 -0500

    update

--- test_cmgp.py
+++ test_cmgp.py
@@ -1,42 +1,44 @@
 from cmgp import CMGP
 from cmgp.datasets import load
 from cmgp.utils.metrics import sqrt_PEHE_with_diff, sqrt_PEHE
-from GPy.util.multioutput import build_XY
-import pandas as pd
-import numpy as np
+from cmgp.utils.random import enable_reproducible_results
 
 X_train, W_train, Y_train, Y_train_full, X_test, Y_test = load("ihdp")
 
-# dim = len(X_train[0])
-# Dataset = pd.DataFrame(X_train)
-# Dataset["Y"] = Y_train
-# Dataset["T"] = W_train
+# repeat across multiple random seeds
+for i in range(3):
+    enable_reproducible_results(i)
+    model = CMGP(X_train, W_train, Y_train, max_gp_iterations=100)
 
-# Feature_names = list(range(dim))
+    pred = model.predict(X_test)
 
-# Dataset0 = Dataset[Dataset["T"] == 0].copy()
-# Dataset1 = Dataset[Dataset["T"] == 1].copy()
+    pehe = sqrt_PEHE_with_diff(Y_test, pred)
 
-# # Extract data for the first learning task (control population)
-# X0 = np.reshape(Dataset0[Feature_names].copy(), (len(Dataset0), dim))
-# y0 = np.reshape(np.array(Dataset0["Y"].copy()), (len(Dataset0), 1))
+    print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
 
-# # Extract data for the second learning task (treated population)
-# X1 = np.reshape(Dataset1[Feature_names].copy(), (len(Dataset1), dim))
-# y1 = np.reshape(np.array(Dataset1["Y"].copy()), (len(Dataset1), 1))
+    pred = model.predict(X_train)
 
-# build_XY([X0, X1], [y0, y1])
+    pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
 
-model = CMGP(X_train, W_train, Y_train, max_gp_iterations=100)
+    print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
 
-pred = model.predict(X_test)
+# repeat across multiple random seeds
+for i in range(3):
+    enable_reproducible_results(i)
+    model = CMGP(X_train,
+                 W_train,
+                 Y_train,
+                 sparse_mode='sgpr',
+                 max_gp_iterations=100)
 
-pehe = sqrt_PEHE_with_diff(Y_test, pred)
+    pred = model.predict(X_test)
 
-print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
+    pehe = sqrt_PEHE_with_diff(Y_test, pred)
 
-pred = model.predict(X_train)
+    print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
 
-pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+    pred = model.predict(X_train)
 
-print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
+    pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+
+    print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
