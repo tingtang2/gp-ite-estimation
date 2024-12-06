1d37b87fbfd0a010259cdfc79889885e36d72e8c
Ting Chen <zc2666@columbia.edu>
2024-12-06 16:04:48 -0500

    final commit for now

--- test_cmgp.py
+++ test_cmgp.py
@@ -1,44 +1,79 @@
 from cmgp import CMGP
 from cmgp.datasets import load
-from cmgp.utils.metrics import sqrt_PEHE_with_diff, sqrt_PEHE
+from cmgp.utils.metrics import sqrt_PEHE_with_diff
 from cmgp.utils.random import enable_reproducible_results
 
 X_train, W_train, Y_train, Y_train_full, X_test, Y_test = load("ihdp")
 
-# repeat across multiple random seeds
-for i in range(3):
-    enable_reproducible_results(i)
-    model = CMGP(X_train, W_train, Y_train, max_gp_iterations=100)
 
-    pred = model.predict(X_test)
+def main_results():
+    # repeat across multiple random seeds
+    for i in range(3):
+        enable_reproducible_results(i)
+        model = CMGP(X_train, W_train, Y_train, max_gp_iterations=100)
 
-    pehe = sqrt_PEHE_with_diff(Y_test, pred)
+        pred = model.predict(X_test)
 
-    print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
+        pehe = sqrt_PEHE_with_diff(Y_test, pred)
 
-    pred = model.predict(X_train)
+        print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
 
-    pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+        pred = model.predict(X_train)
 
-    print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
+        pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
 
-# repeat across multiple random seeds
-for i in range(3):
-    enable_reproducible_results(i)
-    model = CMGP(X_train,
-                 W_train,
-                 Y_train,
-                 sparse_mode='sgpr',
-                 max_gp_iterations=100)
+        print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
 
-    pred = model.predict(X_test)
+    # repeat across multiple random seeds
+    for i in range(3):
+        enable_reproducible_results(i)
+        model = CMGP(X_train,
+                     W_train,
+                     Y_train,
+                     sparse_mode='sgpr',
+                     max_gp_iterations=100)
 
-    pehe = sqrt_PEHE_with_diff(Y_test, pred)
+        pred = model.predict(X_test)
 
-    print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
+        pehe = sqrt_PEHE_with_diff(Y_test, pred)
 
-    pred = model.predict(X_train)
+        print(f"out of sample PEHE score for CMGP on ihdp = {pehe}")
 
-    pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+        pred = model.predict(X_train)
 
-    print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
+        pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+
+        print(f"in sample PEHE score for CMGP on ihdp = {pehe}")
+
+
+def ablate_ip():
+    inducing_points = [50]
+
+    for ip in inducing_points:
+        model = CMGP(X_train,
+                     W_train,
+                     Y_train,
+                     num_inducing_points=ip,
+                     sparse_mode='sgpr',
+                     max_gp_iterations=100)
+
+        pred = model.predict(X_test)
+
+        pehe = sqrt_PEHE_with_diff(Y_test, pred)
+
+        print(
+            f"out of sample PEHE score for CMGP on ihdp, with {ip} inducing points = {pehe}"
+        )
+
+        pred = model.predict(X_train)
+
+        pehe = sqrt_PEHE_with_diff(Y_train_full, pred)
+
+        print(
+            f"in sample PEHE score for CMGP on ihdp, with {ip} inducing points = {pehe}"
+        )
+        del model
+
+
+ablate_ip()
+# main_results()
